f5f7734e4d3645463d3d4a9c00213de503a66e1c
Kevin Low <kevin@lowones.com>
2025-06-11 10:27:02 -0400

    change port and listen external

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+#    app.run(debug=True)
+#    app.run(debug=True, port=6666)
+    app.run(debug=True, port=6666, host='0.0.0.0')
+#    app.run(debug=True, host='0.0.0.0', port=5555)
